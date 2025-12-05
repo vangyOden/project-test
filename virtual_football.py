eac33d145a3107fc1f650b2046a2f489606cd5f7
Bright1017 <melvinbright101@gmail.com>
2025-12-05 17:20:31 +0100

    added match_engine functions which stimulates match activities in real time.

--- virtual_football.py
+++ virtual_football.py
@@ -45,3 +45,27 @@ def match_engine(current_minute):
     else:
         match_status(f"{activity} for {team}", current_minute)
 
+# this code displays match kickoff with team names, time
+def start_match():
+    print(f"[00:00] Match started between {home_team_name} and {away_team_name}")
+
+# this function displays the final match status when the game is over
+def end_match():
+    print(f"[90:00] Match ended.")
+    print(f"Final Match Scores: {home_team_name} {away_team_name} - {home_team_score} {away_team_score}")
+
+def run_match():
+    start_match()
+
+    total_real_seconds = runtime_minutes * 60
+    time_step = total_real_seconds / match_duration
+
+    for minute in range(match_duration): # this code slows down match time to stimulate real life game activity
+        time.sleep(time_step)
+         
+        if random.random() < 0.3:  # this code indicates that there will a 30% chance an activity to happen
+            match_engine(minute)   # within the match timeframe 
+
+    end_match()
+
+run_match()
